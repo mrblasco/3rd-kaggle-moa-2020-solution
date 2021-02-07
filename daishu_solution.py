ae4124e7c60d38ee54bcdd321ece9f27b54305a8
Andrea Blasco <ablasco@fas.harvard.edu>
2021-02-07 09:40:23 -0500

    save oof

--- daishu_solution.py
+++ daishu_solution.py
@@ -5,57 +5,36 @@ import gc,os
 from time import time
 import datetime,random
 from tqdm import tqdm
+
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import KFold, StratifiedKFold,GroupKFold
+from sklearn.model_selection import KFold, StratifiedKFold, GroupKFold
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import QuantileTransformer
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset,TensorDataset, DataLoader,RandomSampler
+from torch.utils.data import Dataset, TensorDataset, DataLoader, RandomSampler
+from torch.nn.modules.loss import _WeightedLoss
+
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 import argparse
 import json
 import utils
 import logging
 
-
-# Usage -------------------------------------------------------
-# Inputs: 
-#   --input_dir 
-# Parameters:
-#   EPOCHS = 23
-#   ncompo_cells = 10
-#   ncompo_genes = 80
-#   dropout = 0.26 (probability)
-# Outputs: 
-#   ./daishu_submission.csv
+# Functions -------------------------------------------------------
 
 def Parse_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--input_dir', default='./data/from_kaggle', help='Directory containing dataset')
+    args.add_argument('--input_dir', default='./data/from_kaggle'
+                    , help='Directory containing dataset')
     args.add_argument('--model_dir', default='./experiments/base_model'
                       , help='Directory containing params.json')
     args = args.parse_args()
     return args
-
-args = Parse_args()
-
-json_path = os.path.join(args.model_dir, 'params.json')   
-assert os.path.isfile(json_path), "No json file found at {}".format(json_path)
-params = utils.Params(json_path)
-
-warnings.simplefilter('ignore')
-
-ncompo_genes = params.ncompo_genes #80
-ncompo_cells = params.ncompo_cells #10
-
-device = ('cuda' if torch.cuda.is_available() else 'cpu')
-EPOCHS = params.num_epochs # 23
-
-utils.set_logger(os.path.join(args.model_dir, 'train.log'))
 
 def Seed_everything(seed=42):
     random.seed(seed)
@@ -65,9 +44,7 @@ def Seed_everything(seed=42):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
-Seed_everything(seed=42)
-
-def Metric(labels,preds):
+def Metric(labels, preds):
     labels = np.array(labels)
     preds = np.array(preds)
     metric = 0
@@ -75,67 +52,11 @@ def Metric(labels,preds):
         metric += (-np.mean(labels[:,i]*np.log(np.maximum(preds[:,i],1e-15))+(1-labels[:,i])*np.log(np.maximum(1-preds[:,i],1e-15))))
     return metric/labels.shape[1]
 
-files = ['%s/test_features.csv'%args.input_dir,
-         '%s/train_targets_scored.csv'%args.input_dir,
-         '%s/train_features.csv'%args.input_dir,
-         '%s/train_targets_nonscored.csv'%args.input_dir,
-         '%s/train_drug.csv'%args.input_dir,
-         '%s/sample_submission.csv'%args.input_dir]
- 
-logging.info("Loading the datasets from {}".format(args.input_dir))  
-
-test            = pd.read_csv(files[0])
-train_target    = pd.read_csv(files[1])
-train           = pd.read_csv(files[2])
-train_nonscored = pd.read_csv(files[3])
-train_drug      = pd.read_csv(files[4])
-sub             = pd.read_csv(files[5])
-
-# If testing, keep only first 5,000 rows 
-if params.use_subset == "True": 
-    train           = train.iloc[:5000, ]
-
-# keep datasets aligned by rows (axis = 0) 
-(train_target, _)     = train_target.align(train, axis = 0, join = 'inner')
-(train_nonscored, _)  = train_nonscored.align(train, axis = 0, join = 'inner')
-(train_drug, _)       = train_drug.align(train, axis = 0, join = 'inner')
-
-logging.info("Training dataset of size {} x {}".format(train.shape[0], train.shape[1]))
-logging.info("Training targets of size {} x {}".format(train_target.shape[0], train_target.shape[1]))
-logging.info("Testing dataset of size {} x {}".format(test.shape[0], test.shape[1]))
-
-logging.info("Dropping selected features with low variability...")  
-
-drop_cols = ['g-513', 'g-370', 'g-707', 'g-300', 'g-130', 'g-375', 'g-161',
-       'g-191', 'g-376', 'g-176', 'g-477', 'g-719', 'g-449', 'g-204',
-       'g-595', 'g-310', 'g-276', 'g-399', 'g-438', 'g-537', 'g-582',
-       'g-608', 'g-56', 'g-579', 'g-45', 'g-252', 'g-12', 'g-343',
-       'g-737', 'g-571', 'g-555', 'g-506', 'g-299', 'g-715', 'g-239',
-       'g-654', 'g-746', 'g-436', 'g-650', 'g-326', 'g-630', 'g-465',
-       'g-487', 'g-290', 'g-714', 'g-452', 'g-227', 'g-170', 'g-520',
-       'g-467']+['g-54', 'g-87', 'g-111', 'g-184', 'g-237', 'g-302', 'g-305',
-       'g-313', 'g-348', 'g-399', 'g-450', 'g-453', 'g-461', 'g-490',
-       'g-497', 'g-550', 'g-555', 'g-584', 'g-592', 'g-682', 'g-692',
-       'g-707', 'g-748', 'g-751']
-drop_cols = list(set(drop_cols))
-train.drop(drop_cols,axis=1,inplace=True)
-test.drop(drop_cols,axis=1,inplace=True)
-
-genes = [col for col in train.columns if col.startswith("g-")]
-cells = [col for col in train.columns if col.startswith("c-")]
-
-features = genes + cells
-targets = [col for col in train_target if col!='sig_id']
-nonscored_targets = [col for col in train_nonscored if col!='sig_id']
-
-ori_train = train.copy()
-ctl_train = train.loc[train['cp_type']=='ctl_vehicle'].append(test.loc[test['cp_type']=='ctl_vehicle']).reset_index(drop=True)
-
 def Feature(df):
     transformers = {}
     for col in (genes+cells):
-        transformer = QuantileTransformer(n_quantiles=100,random_state=0, output_distribution='normal')
-        transformer.fit(df[:train.shape[0]][col].values.reshape(-1,1))#transformer.fit(df[col].values.reshape(-1,1))
+        transformer = QuantileTransformer(n_quantiles=100, random_state=0, output_distribution='normal')
+        transformer.fit(df[:train.shape[0]][col].values.reshape(-1,1))
         df[col] = transformer.transform(df[col].values.reshape(-1,1)).reshape(1,-1)[0]
         transformers[col] = transformer
     gene_pca = PCA(n_components = ncompo_genes,
@@ -152,20 +73,6 @@ def Feature(df):
         df = pd.concat([df,tmp],axis=1)
         df.drop([col],axis=1,inplace=True)
     return df,transformers,gene_pca,cell_pca
-
-logging.info("Processing features...")
-
-tt = train.append(test).reset_index(drop=True)
-tt,transformers,gene_pca,cell_pca = Feature(tt)
-train = tt[:train.shape[0]]
-test = tt[train.shape[0]:].reset_index(drop=True)
-
-if 1:
-    train_target = train_target.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
-    train_nonscored = train_nonscored.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
-    train_drug = train_drug.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
-    ori_train = ori_train.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
-    train = train.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
 
 def Ctl_augment(train,target,train_nonscored):
     aug_trains = []
@@ -209,10 +116,6 @@ def Ctl_augment(train,target,train_nonscored):
     ys = target[targets].values
     ys1 = target[nonscored_targets].values
     return xs,ys,ys1
-
-import torch
-from torch.nn.modules.loss import _WeightedLoss
-import torch.nn.functional as F
 
 class SmoothBCEwLogits(_WeightedLoss):
     def __init__(self, weight=None, reduction='mean', smoothing=0.0):
@@ -535,7 +438,8 @@ def train_and_predict(features, sub, aug, mn,  folds=5, seed=6):
         oof[t] = 0.0
     preds = []
     test_X = test[features].values
-    test_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(test_X)),batch_size=128,shuffle=False)
+    test_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(test_X))
+                                , batch_size=128, shuffle=False)
     eval_train_loss = 0
     for fold, (trn_ind, val_ind) in enumerate(MultilabelStratifiedKFold(n_splits = folds, shuffle=True, random_state=seed)\
                                               .split(train, train_target[targets])):
@@ -555,9 +459,15 @@ def train_and_predict(features, sub, aug, mn,  folds=5, seed=6):
         valid_X = train.loc[val_ind,features].values
         valid_Y = train_target.loc[val_ind,targets].values
 
-        eval_train_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(eval_train_X),torch.Tensor(eval_train_Y)),batch_size=128,shuffle=False, drop_last=False)
-        train_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(train_X),torch.Tensor(train_Y),torch.Tensor(train_Y1)),batch_size=128,shuffle=True, drop_last=True)
-        valid_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(valid_X),torch.Tensor(valid_Y)),batch_size=1024,shuffle=False)
+        eval_train_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(eval_train_X)
+                                            , torch.Tensor(eval_train_Y))
+                                            , batch_size=128,shuffle=False, drop_last=False)
+        train_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(train_X)
+                                      , torch.Tensor(train_Y), torch.Tensor(train_Y1))
+                                      , batch_size=128,shuffle=True, drop_last=True)
+        valid_data_loader = DataLoader(dataset=TensorDataset(torch.Tensor(valid_X)
+                                      , torch.Tensor(valid_Y))
+                                      , batch_size=1024, shuffle=False)
 
         if mn == 'tabnet':
             model = TabNet(len(features),len(targets),len(nonscored_targets), n_d=128,n_a=256,n_shared=1,n_ind=1,n_steps=3,relax=2.,vbs=128)
@@ -679,8 +589,101 @@ def train_and_predict(features, sub, aug, mn,  folds=5, seed=6):
     print('eval_train_loss:', eval_train_loss/folds
                             , oof[targets].mean().mean()
                             , sub[targets].mean().mean())
-    print('valid_metric:%.6f'%Metric(train_target[targets].values,oof[targets].values))
+    logging.info('valid_metric:%.6f'%Metric(train_target[targets].values,oof[targets].values))
     return oof,sub
+
+
+# Main -------------------------------------------------------
+
+args = Parse_args()
+
+# load parameters 
+json_path = os.path.join(args.model_dir, 'params.json')   
+assert os.path.isfile(json_path), "No json file found at {}".format(json_path)
+params = utils.Params(json_path)
+
+#warnings.simplefilter('ignore')
+
+ncompo_genes = params.ncompo_genes #80
+ncompo_cells = params.ncompo_cells #10
+
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+EPOCHS = params.num_epochs # 23
+
+utils.set_logger(os.path.join(args.model_dir, 'train.log'))
+
+Seed_everything(seed=42)
+
+files = ['%s/test_features.csv'%args.input_dir,
+         '%s/train_targets_scored.csv'%args.input_dir,
+         '%s/train_features.csv'%args.input_dir,
+         '%s/train_targets_nonscored.csv'%args.input_dir,
+         '%s/train_drug.csv'%args.input_dir,
+         '%s/sample_submission.csv'%args.input_dir]
+ 
+logging.info("Loading the datasets from {}".format(args.input_dir))  
+
+test            = pd.read_csv(files[0])
+train_target    = pd.read_csv(files[1])
+train           = pd.read_csv(files[2])
+train_nonscored = pd.read_csv(files[3])
+train_drug      = pd.read_csv(files[4])
+sub             = pd.read_csv(files[5])
+
+# keep datasets aligned by rows (axis = 0) 
+(train_target, _)     = train_target.align(train, axis = 0, join = 'inner')
+(train_nonscored, _)  = train_nonscored.align(train, axis = 0, join = 'inner')
+(train_drug, _)       = train_drug.align(train, axis = 0, join = 'inner')
+
+# info 
+logging.info("Training dataset of size {} x {}".format(train.shape[0], train.shape[1]))
+logging.info("Training targets of size {} x {}".format(train_target.shape[0], train_target.shape[1]))
+logging.info("Testing dataset of size {} x {}".format(test.shape[0], test.shape[1]))
+
+logging.info("Dropping selected features with low variability...")  
+
+drop_cols = ['g-513', 'g-370', 'g-707', 'g-300', 'g-130', 'g-375', 'g-161',
+       'g-191', 'g-376', 'g-176', 'g-477', 'g-719', 'g-449', 'g-204',
+       'g-595', 'g-310', 'g-276', 'g-399', 'g-438', 'g-537', 'g-582',
+       'g-608', 'g-56', 'g-579', 'g-45', 'g-252', 'g-12', 'g-343',
+       'g-737', 'g-571', 'g-555', 'g-506', 'g-299', 'g-715', 'g-239',
+       'g-654', 'g-746', 'g-436', 'g-650', 'g-326', 'g-630', 'g-465',
+       'g-487', 'g-290', 'g-714', 'g-452', 'g-227', 'g-170', 'g-520',
+       'g-467']+['g-54', 'g-87', 'g-111', 'g-184', 'g-237', 'g-302', 'g-305',
+       'g-313', 'g-348', 'g-399', 'g-450', 'g-453', 'g-461', 'g-490',
+       'g-497', 'g-550', 'g-555', 'g-584', 'g-592', 'g-682', 'g-692',
+       'g-707', 'g-748', 'g-751']
+drop_cols = list(set(drop_cols))
+train.drop(drop_cols,axis=1,inplace=True)
+test.drop(drop_cols,axis=1,inplace=True)
+
+genes = [col for col in train.columns if col.startswith("g-")]
+cells = [col for col in train.columns if col.startswith("c-")]
+
+features = genes + cells
+targets = [col for col in train_target if col!='sig_id']
+nonscored_targets = [col for col in train_nonscored if col!='sig_id']
+
+# copy original training dataset
+ori_train = train.copy()
+
+# controls in training and testing 
+ctl_train = train.loc[train['cp_type']=='ctl_vehicle'].append(test.loc[test['cp_type']=='ctl_vehicle']).reset_index(drop=True)
+
+logging.info("Processing features...")
+
+tt = train.append(test).reset_index(drop=True)
+tt, transformers, gene_pca, cell_pca = Feature(tt)
+train = tt[:train.shape[0]]
+test = tt[train.shape[0]:].reset_index(drop=True)
+
+# remove controls in training 
+if 1:
+    train_target = train_target.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+    train_nonscored = train_nonscored.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+    train_drug = train_drug.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+    ori_train = ori_train.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
+    train = train.loc[train['cp_type']!='ctl_vehicle'].reset_index(drop=True)
 
 train_cols = [col for col in train.columns if col not in ['sig_id','cp_type']]
 
@@ -688,24 +691,43 @@ logging.info("Model 1: attention DNN (aug = TRUE)...")
 
 seed = 6
 Seed_everything(seed)
-oof,sub = train_and_predict(train_cols,sub.copy(),aug=True,mn='attention_dnn',seed=seed)
+oof, sub = train_and_predict(features = train_cols
+                            , sub     = sub.copy()
+                            , aug     = True
+                            , mn      = 'attention_dnn'
+                            , folds   = params.num_folds
+                            , seed    = seed)
 outputs = []
 for seed in [66,666]:
     Seed_everything(seed)
-    outputs.append(train_and_predict(train_cols,sub.copy()
-                    , aug=True,mn='attention_dnn',seed=seed))
+    outputs.append(train_and_predict(features = train_cols
+                                    , sub = sub.copy()
+                                    , aug = True
+                                    , mn  = 'attention_dnn'
+                                    , folds = params.num_folds
+                                    , seed=seed))
 
 logging.info("Model 2: TabNet (aug = TRUE)...")
 
-for seed in [8,88,888]:
+for seed in [8, 88, 888]:
     Seed_everything(seed)
-    outputs.append(train_and_predict(train_cols,sub.copy(),aug=True,mn='tabnet',seed=seed))
+    outputs.append(train_and_predict(features = train_cols
+                                    , sub = sub.copy()
+                                    , aug = True
+                                    , mn = 'tabnet'
+                                    , folds = params.num_folds
+                                    , seed = seed))
 
 logging.info("Model 3: DNN (aug = TRUE)...")
 
-for seed in [9,99,999]:
+for seed in [9, 99, 999]:
     Seed_everything(seed)
-    outputs.append(train_and_predict(train_cols,sub.copy(),aug=True,mn='dnn',seed=seed))
+    outputs.append(train_and_predict(features = train_cols
+                                    , sub = sub.copy()
+                                    , aug = True
+                                    , mn = 'dnn'
+                                    , folds = params.num_folds
+                                    , seed = seed))
 
 logging.info("Averaging model predictions ...")
 
@@ -715,11 +737,15 @@ for i, output in enumerate(outputs):
 oof[targets] /= (1+len(outputs))
 sub[targets] /= (1+len(outputs))
 
-valid_metric = Metric(train_target[targets].values,oof[targets].values)
+# compute column-wise log-loss
+valid_metric = Metric(train_target[targets].values, oof[targets].values)
+logging.info('oof mean:%.6f,sub mean:%.6f,valid metric:%.6f'%(oof[targets].mean().mean(), sub[targets].mean().mean(), valid_metric))
 
-logging.info('oof mean:%.6f,sub mean:%.6f,valid metric:%.6f'%(oof[targets].mean().mean(),sub[targets].mean().mean(),valid_metric))
-sub.loc[test['cp_type']=='ctl_vehicle',targets] = 0.0
 fname = os.path.join(args.model_dir, "daishu_submission.csv")
+sub.loc[test['cp_type']=='ctl_vehicle', targets] = 0.0
 sub.to_csv(fname, index=False)
+
+fname = os.path.join(args.model_dir, "daishu_oof.csv")
+oof.to_csv(fname, index=False)
 
 logging.info("done!")

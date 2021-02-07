007316dfb7eeac80fa0a978b0af441bc2ddb2475
Andrea Blasco <ablasco@fas.harvard.edu>
2021-02-07 09:42:26 -0500

    add logger

--- utils.py
+++ utils.py
@@ -64,4 +64,3 @@ def set_logger(log_path):
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(logging.Formatter('%(message)s'))
         logger.addHandler(stream_handler)
-

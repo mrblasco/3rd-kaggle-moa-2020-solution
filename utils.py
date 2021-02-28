fe3fe08ed2068f6b61c4d2c01b74606d5120e7fc
Andrea Blasco <ablasco@fas.harvard.edu>
2021-02-28 13:21:18 -0500

    utils with boolean params

--- utils.py
+++ utils.py
@@ -16,22 +16,26 @@ class Params():
     params.learning_rate = 0.5  # change the value of learning_rate in params
     ```
     """
-
+    
     def __init__(self, json_path):
         with open(json_path) as f:
             params = json.load(f)
-            self.__dict__.update(params)
-
+            self.__dict__.update(params)    
+            
+            for i, j in self.dict.items():
+              if j in ['False', 'True']:
+                self.dict[i] = bool(i)
+    
     def save(self, json_path):
         with open(json_path, 'w') as f:
             json.dump(self.__dict__, f, indent=4)
-
+    
     def update(self, json_path):
         """Loads parameters from json file"""
         with open(json_path) as f:
             params = json.load(f)
             self.__dict__.update(params)
-
+    
     @property
     def dict(self):
         """Gives dict-like access to Params instance by `params.dict['learning_rate']"""
@@ -64,3 +68,6 @@ def set_logger(log_path):
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(logging.Formatter('%(message)s'))
         logger.addHandler(stream_handler)
+
+
+
